2910fe7c9290c262a3c28b0e68446ed8d5d73870
Rayssa <60720014+Rayssamell@users.noreply.github.com>
2023-02-14 18:05:28 -0300

    Delete codigo/usuarios directory

--- codigo/usuarios/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('cadastro/', views.cadastro, name="cadastro"),
-    path('login/', views.logar, name="login"),
-    path('sair/', views.sair, name="sair" )
-]
